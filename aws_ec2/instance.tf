9e7a3744fb736ca58bb97994314d410b0798cc5a
kuganveluswamy <kuganveluswamy@gmail.com>
2021-05-02 15:07:29 -0700

    split contents: instance.tf provider.tf vars.tf for managebility

--- aws_ec2/instance.tf
+++ aws_ec2/instance.tf
@@ -1,16 +1,3 @@
-variable "ACCESS_KEY" {
-    type = string
-}
-variable "SECRET_KEY" {
-    type = string
-}
-
-provider "aws" {
-  access_key = var.ACCESS_KEY
-  secret_key = var.SECRET_KEY
-  region     = "us-west-1"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-0ce448b1704085256"
   instance_type = "t2.micro"
---
+++ aws_ec2/vars.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY" {
+    type = string
+}
+variable "AWS_SECRET_KEY" {
+    type = string
+}
+variable "AWS_REGION" {
+    default = "us-west-1"
+}
